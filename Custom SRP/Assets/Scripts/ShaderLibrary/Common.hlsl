42cc8a24ebee4a6869143e774864c1b8e9f671ee
Artem <34547317+Zenslash@users.noreply.github.com>
2022-07-21 15:40:29 +0300

    Custom Unlit shader

---
+++ Custom SRP/Assets/Scripts/ShaderLibrary/Common.hlsl
@@ -0,0 +1,19 @@
+#ifndef COMMON_INCLUDED
+#define COMMON_INCLUDED
+
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
+#include "UnityInput.hlsl"
+
+#define UNITY_MATRIX_M unity_ObjectToWorld;
+#define UNITY_MATRIX_I_M unity_WorldToObject
+real4 unity_WorldTransformParams;
+
+#define UNITY_MATRIX_V unity_MatrixV
+#define UNITY_MATRIX_VP unity_MatrixVP
+#define UNITY_PREV_MATRIX_M unity_PrevMatrixM;
+#define UNITY_PREV_MATRIX_I_M unity_PrevMatrixIM;
+#define UNITY_MATRIX_P glstate_matrix_projection
+
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/SpaceTransforms.hlsl"
+
+#endif
---
+++ Custom SRP/Assets/Scripts/ShaderLibrary/UnityInput.hlsl
@@ -0,0 +1,14 @@
+#ifndef UNITY_INPUT_INCLUDED
+#define UNITY_INPUT_INCLUDED
+
+
+float4x4 unity_ObjectToWorld;
+float4x4 unity_WorldToObject;
+
+float4x4 unity_MatrixVP;
+float4x4 unity_MatrixV;
+float4x4 unity_PrevMatrixM;
+float4x4 unity_PrevMatrixIM;
+float4x4 glstate_matrix_projection;
+
+#endif
